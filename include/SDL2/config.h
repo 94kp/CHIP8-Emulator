db0a285a63d246255209be85f8c902306debb7cc
Karan <karan994patil@outlook.com>
2020-09-06 10:53:10 +0530

    modified header files, added some new variables

--- include/SDL2/config.h
+++ include/SDL2/config.h
@@ -2,7 +2,9 @@
 #define CONFIG_H
 #endif
 
+
+#define EMULATOR_WINDOW_TITLE "Chip8 Emulator"
 #define CHIP8_MEMORY_SIZE 4096
 #define CHIP8_WIDTH 64
 #define CHIP8_HEIGHT 32
-#endif
+#define CHIP8_WINDOW_MULTIPLIER 10
