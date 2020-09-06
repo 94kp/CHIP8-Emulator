68be71bd2cc67a9b088ea3a386b92ef0d365bda7
Karan <karan994patil@outlook.com>
2020-09-06 10:54:39 +0530

    Fixed misplaced endif

--- include/SDL2/config.h
+++ include/SDL2/config.h
@@ -1,6 +1,5 @@
 #ifndef CONFIG_H
 #define CONFIG_H
-#endif
 
 
 #define EMULATOR_WINDOW_TITLE "Chip8 Emulator"
@@ -8,3 +7,5 @@
 #define CHIP8_WIDTH 64
 #define CHIP8_HEIGHT 32
 #define CHIP8_WINDOW_MULTIPLIER 10
+
+#endif
